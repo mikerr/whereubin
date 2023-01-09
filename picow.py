f7374f9cdb5a5921e2ca018834575296c6679a09
Mike Redrobe <mike@redrobe.net>
2023-01-09 11:24:18 +0000

    Update picow.py
    Now emails a google route map of movement

--- picow.py
+++ picow.py
@@ -3,45 +3,74 @@
 # when in home wifi 
 
 import network, urequests
-import binascii, json
+import binascii,json, time
+import umail 
+# https://github.com/shawwwn/uMail
 
-ssid = 'yourSSID'
+# Home wifi credentials
+ssid = ''
 password = ''
+ 
+GMAIL_USERNAME = '' 
+GMAIL_PASSWORD = ''  
 
 # Google location API : https://developers.google.com/maps/documentation/geolocation/overview
 APIKEY  = "" #YOUR_API_KEY
+    
+def wifi2macs (networks) :
+    # convert networks to json list of mac addresses and signal strengths
+    wifidata = ""
+    for w in networks[:3]: # only top 3 signal strengths
+          hex = binascii.hexlify(w[1]).decode()
+          macaddr = ':'.join(hex[i]+hex[i+1] for i in range(0, len(hex), 2))
+          signal = w[3]
+          macjson =  f'"macAddress": "{macaddr}", "signalStrength": {signal}'
+          wifidata += '{' + macjson + '},'
+
+    wifidata = wifidata[:-1] # strip off trailing comma   
+    return (wifidata)
 
 # scan our networks
 
 wlan = network.WLAN()
 wlan.active(True)
-networks = wlan.scan() 
 
-wifidata = ""
-for w in networks:
-      hex = binascii.hexlify(w[1]).decode()
-      macaddr = ':'.join(hex[i]+hex[i+1] for i in range(0, len(hex), 2))
-      signal = w[3]
-      macjson =  f'"macAddress": "{macaddr}", "signalStrength": {signal}'
-      wifidata += '{' + macjson + '},'
+wifis = []
+oldw1 = ""
 
-wifidata = wifidata[:-1] # strip off trailing comma
+print ("scanning")
+while (len(wifis) < 2):
+    networks = wlan.scan()
+    networks.sort(key=lambda x:x[3],reverse=True) # sorted on RSSI (3)
+    w1 = networks[0][0]
+    if (w1 != oldw1): # only log when strongest wifi changes i.e. we moved away
+        wifis.append (wifi2macs(networks))
+    oldw1 = w1
     
 # connect to our network
 wlan = network.WLAN(network.STA_IF)
 wlan.connect(ssid, password)
 while wlan.isconnected() == False:
         print('Waiting for connection...')
-        sleep(1)
-        
-payload = '{"considerIp": "false",  "wifiAccessPoints": [ ' + wifidata +  ']}'
-headers = {'Content-Type':'application/json'}
-url = f"https://www.googleapis.com/geolocation/v1/geolocate?key={APIKEY}"
-response = urequests.post(url,data=payload,headers = headers)
+        time.sleep(5)
 
-locdata = json.loads(response.content.decode())
-lat = (locdata['location']['lat'])
-lng = (locdata['location']['lng'])
+# geolocate each group of wifi ssids into lat/long
+# then use them as points on a google maps route
 
-print (f"https://maps.google.com/?q={lat},{lng}")
-print (f"https://google.com/maps/{lat},{lng}")
+gmap = "https://google.com/maps/dir/"
+for wifidata in wifis:
+    payload = '{"considerIp": "false",  "wifiAccessPoints": [ ' + wifidata +  ']}'
+    headers = {'Content-Type':'application/json'}
+    url = f"https://www.googleapis.com/geolocation/v1/geolocate?key={APIKEY}"
+    response = urequests.post(url,data=payload,headers=headers)
+    
+    locdata = json.loads(response.content.decode())
+    print (locdata)
+    gmap += str(locdata['location']['lat']) + ',' + str(locdata['location']['lng']) + '/'
+
+smtp = umail.SMTP('smtp.gmail.com', 587, username=GMAIL_USERNAME, password=GMAIL_PASSWORD)
+smtp.to(GMAIL_USERNAME)
+smtp.send("\n" + gmap)
+smtp.quit()
+
+print (gmap)    
