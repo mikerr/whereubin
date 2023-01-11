3437a26377f2585d57b61a38598dbfbbc50701ac
Mike Redrobe <mike@redrobe.net>
2023-01-11 10:32:31 +0000

    Update esp32.ino

--- esp32.ino
+++ esp32.ino
@@ -5,41 +5,17 @@
 #include <ESP_Mail_Client.h>
 // https://github.com/mobizt/ESP-Mail-Client
 
-const char *ssid = "";
+const char *homessid = "";
 const char *password = "";
 String APIKEY = "";
 
 #define GMAIL_USERNAME "@gmail.com" 
 #define GMAIL_PASSWORD ""
 
-WiFiClientSecure client;
-SMTPSession smtp;
+String geolocate(String jsonlist) {
 
-void setup()
-{
-    Serial.begin(115200);
-
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-    WiFi.begin(ssid, password);
-
-    client.setInsecure();
-}
-
-void loop()
-{
-  String jsonlist;
-
-  // do a scan and log wifi APs
-  int n = WiFi.scanNetworks();
-  if (n > 2) // need 3 to geolocate
-   for (int i = 0; i < n; ++i) {
-    String macaddr = WiFi.BSSIDstr(i);
-    int signal = WiFi.RSSI(i);
-    Serial.println(WiFi.SSID(i));
-    jsonlist += "{\"macAddress\": \"" + macaddr + "\", \"signalStrength\": " + signal + "},"; 
-  }
-  jsonlist = jsonlist.substring(0,jsonlist.length() - 1); // remove trailing comma
+  WiFiClientSecure client;
+  client.setInsecure();
 
   const char* server = "www.googleapis.com";
   String url = "/geolocation/v1/geolocate?key=" + APIKEY;
@@ -49,7 +25,6 @@ void loop()
       String body = "{\"considerIp\": \"false\",  \"wifiAccessPoints\": [ " + jsonlist +  "]}";
       int body_len = body.length();
 
-      Serial.println(); Serial.print("Sending Georequest");
       client.println("POST " + url +" HTTP/1.1");
       client.print("Host: "); client.println(server);
       client.println("Content-Type: application/json");
@@ -61,17 +36,29 @@ void loop()
 
       while (client.available() == 0);
 
+      String output = "";
       while (client.available()) {
          char c = client.read();
-         Serial.write(c);
+         //Serial.write(c);
+         output += c;
       }
+      int pos = output.indexOf("\r\n\r\n");
+      String json = output.substring(pos,-1);
+      pos = json.indexOf("lat");
+      String lat = json.substring(pos +6, pos + 16);
+      pos = json.indexOf("lng");
+      String lng = json.substring(pos +6, pos + 16);
+      String latlng = lat + "," + lng;
+      return (latlng);
    } else {
       client.stop();
       Serial.println("Connection Failed");
+      return "";
    }
-  String gmap = "https://google.com/maps/dir/"; 
-  Serial.println(gmap);
+}
 
+void sendmail(String subject, String content) {
+  SMTPSession smtp;
   ESP_Mail_Session session;
   session.server.host_name = "smtp.gmail.com" ;
   session.server.port = 587;
@@ -82,13 +69,13 @@ void loop()
   /* Declare the message class */
   SMTP_Message message;
 
-  message.sender.name = "ESP32";
+  message.sender.name = "";
   message.sender.email = GMAIL_USERNAME;
-  message.subject = "Tracking map";
-  message.addRecipient("Microcontrollerslab",GMAIL_USERNAME);
+  message.subject = subject;
+  message.addRecipient("",GMAIL_USERNAME);
 
   //Send simple text message
-  String textMsg = gmap;
+  String textMsg = content;
   message.text.content = textMsg.c_str();
   message.text.charSet = "us-ascii";
   message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit; 
@@ -96,6 +83,44 @@ void loop()
   if (!smtp.connect(&session));
   if (!MailClient.sendMail(&smtp, &message))
     Serial.println("Error sending Email, " + smtp.errorReason());
+}
 
+void setup()
+{
+    Serial.begin(115200);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    WiFi.begin(homessid, password);
+}
+
+int wifis = 0;
+void loop()
+{
+  String jsonlist,ssid;
+
+  // do a scan and log groups of wifi APs 
+  int n = WiFi.scanNetworks();
+  if (n > 2)  {
+    // need 3 to geolocate
+    for (int i = 0; i < n; ++i) {
+      String macaddr = WiFi.BSSIDstr(i);
+      int signal = WiFi.RSSI(i);
+      ssid = WiFi.SSID(i);
+      jsonlist += "{\"macAddress\": \"" + macaddr + "\", \"signalStrength\": " + signal + "},"; 
+      }
+    jsonlist = jsonlist.substring(0,jsonlist.length() - 1); // remove trailing comma
+ 
+    wifis++;
+  }
+  // when on home wifi, upload the journey
+  // process wifi groups into lat/long with google api
+  if (wifis >1) {
+    String latlong = geolocate(jsonlist);
+    String gmap = "https://google.com/maps/dir/" + latlong; 
+    Serial.println(gmap);
+
+    sendmail("Tracking map",gmap);
+  }
   delay(30000);
 }
